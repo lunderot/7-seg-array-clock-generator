8ca4fd2ae3e3d7a8d3dc5506844cb83b0820ad61
lunderot <mattias.lunderot@gmail.com>
2020-05-23 23:31:00 +0200

    Fixed sampling issue

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const samples = [
   [
-    //A
-    { x: 52, y: 36 },
-    { x: 67, y: 36 },
-    { x: 81, y: 36 },
-  ],
-  [
-    //B
-    { x: 97, y: 40 },
-    { x: 94, y: 60 },
-    { x: 91, y: 80 },
-  ],
-  [
-    //C
-    { x: 87, y: 110 },
-    { x: 84, y: 130 },
-    { x: 81, y: 150 },
-  ],
-  [
-    //D
-    { x: 38, y: 151 },
-    { x: 53, y: 151 },
-    { x: 67, y: 151 },
-  ],
-  [
-    //E
-    { x: 26, y: 110 },
-    { x: 23, y: 130 },
-    { x: 20, y: 150 },
+    //G
+    { x: 42, y: 93 },
+    { x: 59, y: 93 },
+    { x: 76, y: 93 },
   ],
   [
     //F
@@ -36,10 +12,34 @@ const samples = [
     { x: 30, y: 80 },
   ],
   [
-    //G
-    { x: 42, y: 93 },
-    { x: 59, y: 93 },
-    { x: 76, y: 93 },
+    //E
+    { x: 26, y: 110 },
+    { x: 23, y: 130 },
+    { x: 20, y: 150 },
+  ],
+  [
+    //D
+    { x: 38, y: 151 },
+    { x: 53, y: 151 },
+    { x: 67, y: 151 },
+  ],
+  [
+    //C
+    { x: 87, y: 110 },
+    { x: 84, y: 130 },
+    { x: 81, y: 150 },
+  ],
+  [
+    //B
+    { x: 97, y: 40 },
+    { x: 94, y: 60 },
+    { x: 91, y: 80 },
+  ],
+  [
+    //A
+    { x: 52, y: 36 },
+    { x: 67, y: 36 },
+    { x: 81, y: 36 },
   ],
   [
     //DP
@@ -61,7 +61,7 @@ window.onload = () => {
   const generateButton = document.getElementById("generate");
   const outputText = document.getElementById("output");
   generateButton.onclick = () => {
-    outputText.value = generate(hiddenCtx, samples, hiddenCanvas, 60);
+    outputText.value = generate(hiddenCtx, samples, hiddenCanvas, 1);
   };
 
   window.requestAnimationFrame(drawWrapper(ctx, canvas));
@@ -71,11 +71,11 @@ const generate = (ctx, samples, canvas, steps) => {
   let array = [];
   for (let s = 0; s < steps; s++) {
     draw(ctx, s / steps);
-    const image = ctx.getImageData(0, 0, canvas.height, canvas.width).data;
-    for (let i = 0; i < 6 * 4; i++) {
-      for (let j = 0; j < 6; j++) {
-        const offset = { x: i * 126 - 10, y: j * 190 };
-        array.push(sampleDigit(image, samples, offset, canvas.width));
+    const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for (let j = 0; j < 6; j++) {
+      for (let i = 0; i < 6 * 4; i++) {
+        const offset = { x: i * 126, y: j * 190 };
+        array.push(sampleDigit(image.data, samples, offset, image.width));
       }
     }
   }
@@ -87,12 +87,13 @@ const generate = (ctx, samples, canvas, steps) => {
         p +
         `0x${value}${i < array.length - 1 ? "," : ""}${addNewline ? "\n" : ""}`
       );
-    }, "byte pattern[] = {") + "};"
+    }, "uint8_t pattern[] = {") + "};"
   );
 };
 
-const sampleImageData = (image, x, y, canvasWidth) =>
-  image[y * (canvasWidth * 4) + x * 4];
+const sampleImageData = (image, x, y, canvasWidth) => {
+  return image[(y * canvasWidth + x) * 4];
+};
 
 const sampleSegment = (image, segment, offset, canvasWidth) => {
   const a = segment.map((samplePoint) =>
@@ -138,11 +139,20 @@ const stripeEffect = (ctx, t) => {
   }
 };
 
+const debugEffect = (ctx, t) => {
+  ctx.fillStyle = "black";
+  ctx.translate(-1260 + t * 126 * 2 * 0, 0);
+  for (let i = 0; i < 100; i++) {
+    ctx.translate(126 * 2, 0);
+    ctx.fillRect(0, -10000, 126, 100000);
+  }
+};
+
 const drawSamples = (ctx, samples) => {
   ctx.fillStyle = "red";
-  for (let i = 0; i < 6 * 4; i++) {
-    for (let j = 0; j < 6; j++) {
-      ctx.setTransform(1, 0, 0, 1, i * 126 - 10, j * 190);
+  for (let j = 0; j < 6; j++) {
+    for (let i = 0; i < 6 * 4; i++) {
+      ctx.setTransform(1, 0, 0, 1, i * 126, j * 190);
       samples.map((digit) =>
         digit.map(({ x, y }) => {
           ctx.fillRect(x, y, 10, 10);
